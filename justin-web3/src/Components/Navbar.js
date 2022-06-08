0c910fe4a26eac85ac6065ce205d0deee8f5b432
justiny2c <justiny2c@gmail.com>
2022-06-07 17:35:56 -0700

    Starting Navbar component

---
+++ justin-web3/src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import {Link} from "react-router-dom"
+import "./Navbar.css"
+
+
+function Navbar() {
+  return (
+    <div>
+        <nav className='navbar'>
+            <div className='nav-bar-container'>
+                {/* <Link to="/" className="navbar-logo"> */}
+                   <h1>JUSTIN</h1>
+                {/* </Link> */}
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
