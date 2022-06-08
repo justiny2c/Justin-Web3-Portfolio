90a064d4067d0b2ff41fda288456c8787e24cd9e
justiny2c <justiny2c@gmail.com>
2022-06-08 12:26:09 -0700

    Header formatted

--- justin-web3/src/Components/Header.js
+++ justin-web3/src/Components/Header.js
@@ -1,23 +1,25 @@
 import React, { useState } from 'react'
-import { Avatar, Grid } from "@nextui-org/react";
+import { Container, Avatar, Row} from "@nextui-org/react";
 import Azuki from "../images/azuki.jpg"
 import Justin from "../images/linkedin.jpg"
 
 
 function Header()   {
     return (
-      <div className='Header'>
-        <Avatar 
-            circular
-            src={Azuki}
-            css={{ size: "10rem" }}
-            />
-        <Avatar 
-            circular
-            src={Justin}
-            css={{ size: "10rem" }}
-            />         
-      </div>
+      <Container className='Header' xl>
+          <Row justify="center">      
+            <Avatar 
+                circular
+                src={Justin}
+                css={{ size: "10rem", margin:"1rem" }}
+                /> 
+            <Avatar 
+                circular
+                src={Azuki}
+                css={{ size: "10rem", margin:"1rem" }}
+                />        
+          </Row>        
+      </Container>
     )
 }
 
